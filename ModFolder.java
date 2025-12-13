1537036d8efed6378c9ae166d9cd67b6cee9a993
Law Abiding Coder <colton.trash5.5@proton.me>
2025-12-13 08:29:17 -0600

    Added `ensureExists` method

--- ModFolder.java
+++ ModFolder.java
@@ -50,4 +50,8 @@ public class ModFolder extends File {
         }
         return null;
     }
+    public boolean ensureExists() throws Exception
+    {
+        return exists() || getParentFile().mkdirs() || createNewFile();
+    }
 }
